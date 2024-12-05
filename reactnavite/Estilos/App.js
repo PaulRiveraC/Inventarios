98504abcbf167bd6afd0916050510fd724aa7acf
PaulRiveraC <paulriveracamacho@gmail.com>
2024-12-05 11:43:37 -0500

    margin

--- reactnavite/Estilos/App.js
+++ reactnavite/Estilos/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Button title="COMPONENTE 1" />
-      <Button title="COMPONENTE 2" color="green"/>
+      <Button title="COMPONENTE 2" color="green" />
       <Button title="COMPONENTE 3" />
 
       <StatusBar style="auto" />
@@ -17,14 +17,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    flexDirection: "column",
-    //flexDirection: "row"
-    //justifyContent: "center"
-    //justifyContent: "flex-star"
-    //justifyContent: "flex-end"
-    //justifyContent: "space-around"
-    //justifyContent: "space-between"
-    justifyContent: "space-evenly"
+    // UNO ABAJO DEL OTRO
+    //flexDirection: "column",
+    //UNO A LADO DEL OTRO 
+    //flexDirection: "row",
+    //al eje principal
+    //justifyContent: "center",
+    //justifyContent: "flex-star",
+    //justifyContent: "flex-end",
+    //justifyContent: "space-around",
+    //justifyContent: "space-between",
+    //justifyContent: "space-evenly",
+
+
+    //al eje secuendario
+    //alignItems: "center",
+    //alignItems:"flex-start"
+    //alignItems: "stretch"
+    //alignItems: "flex-end"
 
 
 
